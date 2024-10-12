e1ad30ef7a51e279d17d7c43158fbc63c870de10
Aishwarya Rana <160381307+Aishwarya2004Github@users.noreply.github.com>
2024-10-12 12:20:59 +0530

    Fixed autoprefixer issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
-        changeOrigin: true,
         secure: false,
       },
     },
   },
-  css: {
-    postcss: {
-      plugins: [autoprefixer, tailwindcss],
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
+  
   plugins: [react()],
+
+  build: {
+    chunkSizeWarningLimit: 1000, // Set the limit in kilobytes (e.g., 1000 KB = 1 MB)
+  },
 });
