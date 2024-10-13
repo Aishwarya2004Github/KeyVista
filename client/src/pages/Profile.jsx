e4709aa7de5fa1dd24d5a7c3d82be1dd51e78d82
Aishwarya Rana <160381307+Aishwarya2004Github@users.noreply.github.com>
2024-10-13 13:05:09 +0530

    deployed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -239,17 +239,23 @@ export default function Profile() {
             Your Listings
           </h1>
           {userListings.map((listing) => (
-            <div
-              key={listing._id}
-              className='border rounded-lg p-3 flex justify-between items-center gap-4'
-            >
-              <Link to={`/listing/${listing._id}`}>
-                <img
-                  src={listing.imageUrls[0]}
-                  alt='listing cover'
-                  className='h-16 w-16 object-contain'
-                />
-              </Link>
+         <div
+         key={listing._id}
+         className='border rounded-lg p-3 flex justify-between items-center gap-4'
+       >
+         <Link to={`/listing/${listing._id}`}>
+           <img
+             src={
+               listing.imageUrls[0]
+                 ? `${process.env.REACT_APP_BACKEND_URL}/uploads/${listing.imageUrls[0]}`
+                 : 'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg'
+             }
+             alt='listing cover'
+             className='h-16 w-16 object-contain'
+           />
+         </Link>
+       
+       
               <Link
                 className='text-slate-700 font-semibold  hover:underline truncate flex-1'
                 to={`/listing/${listing._id}`}
